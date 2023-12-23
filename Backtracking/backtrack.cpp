8975942be639bcfd1cb487538fd5f3942f5723f7
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-23 11:45:04 +0600

    mission accomplished 🔥🚀🔥

---
+++ Backtracking/backtrack.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define all(X) (X).begin(), (X).end()
+#define pub push_back
+#define pob pop_back
+#define nl  '\n'
+#define c(x) cout<<x<<nl
+#define yes cout<<"YES"<<nl
+#define no cout<<"NO"<<nl
+#define Yes cout<<"Yes"<<nl
+#define No cout<<"No"<<nl
+typedef pair<int,int>pii;
+#define forl(ty,var,str,end) for(ty var=str; var<end; var++)
+# define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+bool vis[20];
+void back(ll n){
+    for(ll i=1;i<=n;i++){
+        if(vis[i]==false){
+            cout<<i<<" ";
+            vis[i]=true;
+            back(n);
+            vis[i]=false;
+        }
+    }
+}
+void solve(){
+    ll n;cin>>n;
+    memset(vis,false,sizeof(vis));
+    back(n);
+}
+int main(){
+    FAST;
+    int t;cin>>t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
