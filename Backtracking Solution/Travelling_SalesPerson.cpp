88f3867e86cd1fa1134ecc535e57056ed8668b2c
SunjidaAkter <sanjidabintesayid7@gmail.com>
2023-12-30 14:31:54 +0600

    mission accomplished 🔥🚀🔥

--- Backtracking Solution/Travelling_SalesPerson.cpp
+++ Backtracking Solution/Travelling_SalesPerson.cpp
@@ -13,8 +13,34 @@ using namespace std;
 typedef pair<int,int>pii;
 #define forl(ty,var,str,end) for(ty var=str; var<end; var++)
 # define FAST ios_base :: sync_with_stdio (false) ; cin.tie(0) ; cout.tie(0)
+ll n,mn;
+ll a[15][15];
+bool vis[15];
+void back(ll n,vector<ll>&v){
+    if(v.size()==n){
+        
+    }
+    for(ll i=0;i<n;i++){
+        if(!vis[i]){
+            vis[i]=true;
+            v.push_back(i);
+            back(n,v);
+            vis[i]=false;
+            v.pop_back();
+        }
+    }
+}
 void solve(){
-    
+    cin>>n;
+    for(ll i=0;i<n;i++){
+        for(ll j=0;j<n;j++){
+            cin>>a[i][j];
+        }
+    }
+    memset(vis,false,sizeof(vis));
+    vector<ll>v;
+    mn=INT_MAX;
+    back(n-1,v);
 }
 int main(){
     FAST;
